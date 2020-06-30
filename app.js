73df41fb3902773ba38533db383d32605c84ae72
PranavBhatia <bhatiapranav1996@gmail.com>
2020-06-29 21:04:45 -0400

    Adding Controllers

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.render('404', { docTitle: 'Page not found' });
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.render("404", { docTitle: "Page not found" });
 });
 
 app.listen(3000);

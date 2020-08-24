615e5601619ee1b6887dac6b5f54a96b529f8aac
PranavBhatia <bhatiapranav1996@gmail.com>
2020-08-23 20:53:09 -0400

    Finishing the Database Connection

--- util/database.js
+++ util/database.js
@@ -4,15 +4,26 @@ const password = "eHurOLied8nZunkh";
 const dbname = "bookstoreDB";
 const uri = `mongodb+srv://${username}:${password}@cluster0.smq1g.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
-const MongoConnect = (callback) => {
+let _db;
+
+const mongoConnect = (callback) => {
   MongoClient.connect(uri, { useUnifiedTopology: true })
-    .then((result) => {
+    .then((client) => {
       console.log("Connected");
-      callback(result);
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-module.exports = MongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+
+module.mongoConnect = mongoConnect;
+module.getDb = getDb;

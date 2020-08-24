a6da209a03943f8bd0acecd16e6a319417eb8020
PranavBhatia <bhatiapranav1996@gmail.com>
2020-08-23 20:20:44 -0400

    Installing the MongoDB Driver

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const { Sequelize } = require("sequelize");
+const MongoClient = require("mongodb").MongoClient;
+const username = "pranav_bhatia";
+const password = "eHurOLied8nZunkh";
+const dbname = "bookstoreDB";
+const uri = `mongodb+srv://${username}:${password}@cluster0.smq1g.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
-const sequelize = new Sequelize("node.js-online-shop", "root", "123456", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const MongoConnect = (callback) => {
+  MongoClient.connect(uri, { useUnifiedTopology: true })
+    .then((result) => {
+      console.log("Connected");
+      callback(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = MongoConnect;

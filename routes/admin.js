4829aceb04bee17a765d06f3b56ae83a171a3355
PranavBhatia <bhatiapranav1996@gmail.com>
2020-08-24 20:54:28 -0400

    Making the "Edit" & "Delete" Buttons Work Again

--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,9 @@ const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-router.get("/add-product", adminController.getAddProduct);
+router.get("/products", adminController.getProducts);
 
-// router.get("/products", adminController.getProducts);
+router.get("/add-product", adminController.getAddProduct);
 
 router.post("/add-product", adminController.postAddProduct);
 

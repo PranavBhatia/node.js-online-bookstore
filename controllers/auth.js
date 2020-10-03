73f18f50d6a9beff2156092cf6af90b6bde41659
PranavBhatia <bhatiapranav1996@gmail.com>
2020-10-03 15:39:39 -0400

    Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,14 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn =
-    req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+  // const isLoggedIn =
+  //   req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
   res.redirect("/");
 };

862a84e90d7759cd62f528e06d3c8580700ef01a
PranavBhatia <bhatiapranav1996@gmail.com>
2020-10-22 21:57:01 -0400

    Adding a Tiny Code Improvement

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,17 +40,18 @@ exports.postSignup = (req, res, next) => {
       if (userDocument) {
         return res.redirect("/signup");
       }
-      return bcrypt.hash(password, 12);
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => res.redirect("/login"));
     })
-    .then((hashedPassword) => {
-      const user = new User({
-        email,
-        password: hashedPassword,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then((result) => res.redirect("/login"))
     .catch((err) => console.log(err));
 };
 

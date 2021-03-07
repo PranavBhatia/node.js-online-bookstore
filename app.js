bf24817b5e919a183d3e8d60d03dc45aed40f42d
PranavBhatia <bhatiapranav1996@gmail.com>
2021-03-07 12:23:14 -0500

    bodyParser.urlencoded false

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorageConfig, fileFilter }).single("image"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(

eb433b8a2e1f690dcc31daeb969e66b695910fd5
PranavBhatia <bhatiapranav1996@gmail.com>
2021-03-25 22:21:33 -0400

    Sending & Handling Background Requests

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,17 @@
 function deleteProduct(button) {
   const productId = button.parentNode.querySelector("[name=productId]").value;
   const csrf = button.parentNode.querySelector("[name=_csrf]").value;
+
+  fetch("/admin/product/" + productId, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrf,
+    },
+  })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
